77e9050a0125375878fc5cdd61042a7b4224389f
paulwallis42 <paulwallis42@gmail.com>
2017-03-14 15:38:36 +0000

    finding db records by _id

--- test/finding_test.js
+++ test/finding_test.js
@@ -3,8 +3,10 @@ const SuperHero = require('../models/superheros');
 
 describe('Finding records', function(){
 
+  var hero;
+
   beforeEach(function(done){
-      var hero = new SuperHero({
+      hero = new SuperHero({
       name: 'Batman',
       power: 'Gadgets & Martial Arts',
       rating: 7
@@ -15,7 +17,7 @@ describe('Finding records', function(){
     });
   });
 
-  it('Finds a record from the database', function(done){
+  it('Finds a record from the database by name', function(done){
     // Finding the record from SuperHero model/collection -
     // which returns the 'result' which is injected into the following function.
     SuperHero.findOne({ name: 'Batman' }).then(function(result){
@@ -23,5 +25,13 @@ describe('Finding records', function(){
       done();
     });
   });
+
+  it('Finds a record from the database by ID', function(done){
+    SuperHero.findOne({ _id: hero._id }).then(function(result){
+      assert(result._id.toString() === hero._id.toString());
+      done();
+    });
+  });
+
 
 });
